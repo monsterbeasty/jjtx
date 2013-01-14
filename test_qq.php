ce84662b0ddd5f852eca59bce8e19913e32ac8af
Ping-Chris Zheng <christophercheng1222@gmail.com>
2013-01-14 23:13:28 +0800

    commit master

---
+++ test_qq.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>QQ demo page</title>
+    </head>
+    <body>
+        <a href="qq_login.php"><img src="images/qq_login_logo_large.png"></a>
+    </body>
+</html>

6b77bf4fd6aacbe9f89d8bde47d80a4c61b14b1f
Ping-Chris Zheng <christophercheng1222@gmail.com>
2013-01-31 18:11:02 +0800

    weibo added

--- callback_weibo.php
+++ callback_weibo.php
@@ -2,20 +2,60 @@
 
 include_once 'lib/oauth/weibo.php';
 
-$o = new SaeTOAuthV2($CONFIG['weibo']['oauth']['wb_akey'], $CONFIG['weibo']['oauth']['wb_skey'], $CONFIG['weibo']['oauth']['wb_callback']);
-$o->callback();
+$o = new SaeTOAuthV2($CONFIG['weibo']['oauth']['wb_akey'], $CONFIG['weibo']['oauth']['wb_skey']);
 
-$c = new SaeTClientV2($CONFIG['weibo']['oauth']['wb_akey'], $CONFIG['weibo']['oauth']['wb_skey'], $_SESSION['token']['access_token']);
-$c->set_debug( true );
+if(isset($_REQUEST['code']))
+{
+    $keys = array();
+    $keys['code'] = $_REQUEST['code'];
+    $keys['redirect_uri'] = $CONFIG['weibo']['oauth']['wb_callback'];
+    try
+    {
+        $token = $o->getAccessToken('code', $keys);
+    }
+    catch(OAuthException $e)
+    {
+        
+    }
+}
 
-$ms = $c->home_timeline(); // done
-print_pre($ms);
+if($token)
+{
+    $_SESSION['token'] = $token;
+    setcookie('weibojs_' . $o->client_id, http_build_query($token));
 
-$uid_get = $c->get_uid();
-$uid = $uid_get['uid'];
-print_pre($uid);
+    $c = new SaeTClientV2($CONFIG['weibo']['oauth']['wb_akey'], $CONFIG['weibo']['oauth']['wb_skey'], $_SESSION['token']['access_token']);
+    $ms = $c->home_timeline(); // done
+    $uid_get = $c->get_uid();
+    $uid = $uid_get['uid'];
+    $user_message = $c->show_user_by_id($uid); //根据ID获取用户等基本信息
 
-$user_message = $c->show_user_by_id($uid); //根据ID获取用户等基本信息
-print_pre($user_message);
+    print_pre("@@@@@@@@@@@@@@@@@@@@");
+    print_pre($ms);
+    print_pre("@@@@@@@@@@@@@@@@@@@@");
 
+    print_pre("@@@@@@@@@@@@@@@@@@@@");
+    print_pre($uid);
+    print_pre($uid_get);
+    print_pre("@@@@@@@@@@@@@@@@@@@@");
+
+    print_pre("@@@@@@@@@@@@@@@@@@@@");
+    print_pre($uid);
+    print_pre($user_message);
+    print_pre("@@@@@@@@@@@@@@@@@@@@");
+    
+    $ret = $c->update('sae test'); //发送微博
+    if(isset($ret['error_code']) && $ret['error_code'] > 0)
+    {
+        echo "<p>发送失败，错误：{$ret['error_code']}:{$ret['error']}</p>";
+    }
+    else
+    {
+        echo "<p>发送成功</p>";
+    }
+}
+else
+{
+    echo "Auth failed";
+}
 ?>
--- login_weibo.php
+++ login_weibo.php
@@ -2,8 +2,9 @@
 
 include_once 'lib/oauth/weibo.php';
 
-$o = new SaeTOAuthV2($CONFIG['weibo']['oauth']['wb_akey'], $CONFIG['weibo']['oauth']['wb_skey'], $CONFIG['weibo']['oauth']['wb_callback']);
-$o->login();
+$o = new SaeTOAuthV2($CONFIG['weibo']['oauth']['wb_akey'], $CONFIG['weibo']['oauth']['wb_skey']);
+$code_url = $o->getAuthorizeURL($CONFIG['weibo']['oauth']['wb_callback']);
 
+header("Location:$code_url");
 ?>
 
